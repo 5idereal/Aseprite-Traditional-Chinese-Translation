e097e1087f9064dd136da4eb4d807a3d4da5efb0
TakWolf <takwolf@foxmail.com>
2023-10-06 16:09:25 +0800

    升级依赖并修复调用

--- update.py
+++ update.py
@@ -12,16 +12,15 @@ data_dir = os.path.join(project_root_dir, 'data')
 
 
 def main():
-    strings_en = Aseini.pull_strings()
-    strings_en.fallback(Aseini.pull_strings('v1.3-rc6'))
+    strings_en = Aseini.pull_strings('v1.3-rc6')
     strings_en.fallback(Aseini.pull_strings('v1.2.40'))
     strings_en.save(os.path.join(strings_dir, 'en.ini'))
-    logger.info("Update strings: 'en'")
+    logger.info("Update strings: 'en.ini'")
 
     zh_hant_file_path = os.path.join(data_dir, 'zh-hant.ini')
     strings_zh_hant = Aseini.load(zh_hant_file_path)
     strings_zh_hant.save(zh_hant_file_path, strings_en)
-    logger.info("Update strings: 'zh-hant'")
+    logger.info("Update strings: 'zh-hant.ini'")
 
     translated, total = strings_zh_hant.coverage(strings_en)
     progress = translated / total
